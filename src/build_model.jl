56906d982ed6b41dc2e8d2cb84bb72f6216bdbd7
jz <james.zuccollo@epi.org.uk>
2023-05-09 22:32:49 +0100

    Running code

--- src/build_model.jl
+++ src/build_model.jl
@@ -12,9 +12,9 @@ Build the optimisation model for the velogames game.
 """
 function build_model(input_df::DataFrame)
     model = Model(HiGHS.Optimizer)
-    @variable(model, x[input_df.name], Bin)
+    @variable(model, x[input_df.rider], Bin)
     @objective(model, Max, input_df.calc_score' * x) # maximise the total score
-    @constraint(model, input_df.cost' * x <= 100) # cost must be <= 100
+    @constraint(model, input_df.vgcost' * x <= 100) # cost must be <= 100
     @constraint(model, sum(x) == 9) # exactly 9 riders must be chosen
     optimize!(model)
     return value.(x)
--- src/get_data.jl
+++ src/get_data.jl
@@ -1,4 +1,4 @@
-using HTTP, DataFrames, Cascadia, Gumbo, CategoricalArrays, Unicode
+using HTTP, DataFrames, TableScraper, Cascadia, Gumbo, CategoricalArrays, Unicode
 
 const PCS_RANKING_URLS = Dict(
     :overall => "https://www.procyclingstats.com/rankings.php",
@@ -6,58 +6,50 @@ const PCS_RANKING_URLS = Dict(
     :gc => "https://www.procyclingstats.com/rankings/me/gc-ranking",
     :sprint => "https://www.procyclingstats.com/rankings/me/sprinters",
     :mountain => "https://www.procyclingstats.com/rankings/me/climbers",
-    :tt => "https://www.procyclingstats.com/rankings/me/time-trial"
-)
-
-const VG_ROWS = Dict(
-    :name => "https://www.procyclingstats.com/rankings.php",
-    :team => "https://www.procyclingstats.com/rankings/me/one-day-races",
-    :startlist => "https://www.procyclingstats.com/rankings/me/gc-ranking",
-    :cost => "https://www.procyclingstats.com/rankings/me/sprinters",
-    :score => "https://www.procyclingstats.com/rankings/me/climbers",
-    :tt => "https://www.procyclingstats.com/rankings/me/time-trial"
+    :tt => "https://www.procyclingstats.com/rankings/me/time-trial",
 )
 
 """
-## `getvelogamesriders`
-
-This function downloads and parses the rider data from the Velogames website.
-
-The function returns a DataFrame with the following columns:
-
-* `name` - the name of the rider
-* `team` - the team the rider rides for
-* `startlist` - the startlists the rider is on
-* `cost` - the cost of the rider
-* `score` - the number of points scored by the rider in this competition
-* `value` - the number of points scored per unit cost
+`create_key` creates a unique key for each rider based on their name.
 """
-function getvelogamesriders(pageurl::AbstractString)
-    page = HTTP.get(pageurl) |> String |> parsehtml
-    rider_table = eachmatch(sel"table", page.root)[1]
-
-    rider_df = DataFrame(
-        name=String[],
-        cost=Int64[],
-        team=categorical(String[]),
-        score=Int64[],
-        startlist=categorical(String[]),
-    )
-
-    for rider_row in eachmatch(sel"tr", rider_table)[2:end]
-        rider_cells = eachmatch(sel"td", rider_row)
-        push!(rider_df.score, parse(Int64, text(rider_cells[5])))
-        push!(rider_df.name, text(rider_cells[2]))
-        push!(rider_df.team, text(rider_cells[3]))
-        push!(rider_df.cost, parse(Int64, text(rider_cells[6])))
-        push!(rider_df.startlist, text(rider_cells[4]))
+create_key(arr) =
+    map(arr) do x
+        s = replace(x, r"[^a-zA-Z0-9_]" => "")
+        join(
+            sort(
+                collect(
+                    Unicode.normalize(s, stripmark = true, stripcc = true, casefold = true),
+                ),
+            ),
+        )
     end
 
-    # calculate the value of the rider
-    rider_df.value = rider_df.score ./ rider_df.cost
+"""
+## `gettable`
+
+This function downloads and parses the rider data from the Velogames and PCS websites.
+
+The function returns a DataFrame with the columns of the first table on the page. It also adds a (hopefully) unique key for each rider based on their name.
+"""
+function gettable(pageurl::AbstractString)
+    page = scrape_tables(pageurl)
+    rider_df = DataFrame(page[1])
+
+    # lowercase the column names and remove spaces
+    rename!(rider_df, lowercase.(replace.(names(rider_df), " " => "", "#" => "rank")))
+    # rename score to points if it exists
+    if hasproperty(rider_df, :score)
+        rename!(rider_df, :score => :points)
+    end
+    # cast the cost and vgpoints columns to Int64 if they exist
+    for col in [:cost, :points, :rank]
+        if hasproperty(rider_df, col)
+            rider_df[!, col] = parse.(Int64, rider_df[!, col])
+        end
+    end
 
     # add a riderkey column based on the name
-    rider_df.riderkey = create_key(rider_df.name)
+    rider_df.riderkey = create_key(rider_df.rider)
     # check that the riderkey is unique
     @assert length(unique(rider_df.riderkey)) == length(rider_df.riderkey) "Rider keys are not unique"
 
@@ -78,25 +70,7 @@ The function returns a DataFrame with the following columns:
     * `score` - the number of points scored by the rider
 """
 function getpcsriders(pageurl::String)
-    page = parsehtml(read(download(pageurl), String))
-    rider_table = eachmatch(sel"table", page.root)[1]
-    rider_names = String[]
-    rider_team = String[]
-    rider_placings = Int64[]
-    rider_scores = Int64[]
-    for rider_row in eachmatch(sel"tr", rider_table)[2:end]
-        rider_cells = eachmatch(sel"td", rider_row)
-        push!(rider_names, text(rider_cells[2]))
-        push!(rider_team, text(rider_cells[3]))
-        push!(rider_placings, parse(Int64, text(rider_cells[4])))
-        push!(rider_scores, parse(Int64, text(rider_cells[5])))
-    end
-    rider_df = DataFrame(
-        name=rider_names,
-        team=categorical(rider_team),
-        placing=rider_placings,
-        score=rider_scores
-    )
+    rider_df = scrape_tables(pageurl)
 
     return rider_df
 
@@ -119,39 +93,40 @@ function getpcsranking(category::Symbol)
     haskey(PCS_RANKING_URLS, category) || error("Invalid category: $category")
 
     # download the page and parse the table
-    page = HTTP.get(PCS_RANKING_URLS[category]).body |> String |> parsehtml
-    rider_table = eachmatch(sel"table", page.root)[1]
+    page = gettable(PCS_RANKING_URLS[category])
 
-    # parse the table to a DataFrame
-    rider_df = DataFrame(
-        rank=Int64[],
-        name=String[],
-        team=String[],
-        points=Int64[]
-    )
-
-    for rider_row in eachmatch(sel"tr", rider_table)[2:end]
-        rider_cells = eachmatch(sel"td", rider_row)
-        push!(rider_df.rank, parse(Int64, text(rider_cells[1])))
-        push!(rider_df.name, text(rider_cells[4]))
-        push!(rider_df.team, text(rider_cells[5]))
-        push!(rider_df.points, parse(Int64, text(rider_cells[6])))
-    end
-
-    # add a riderkey column based on the name
-    rider_df.riderkey = create_key(rider_df.name)
-
-    # check that the riderkey is unique
-    length(unique(rider_df.riderkey)) == length(rider_df.riderkey) || error("Rider keys are not unique")
+    # filter to rank, rider, team, and points
+    rider_df = page[:, [:rank, :rider, :team, :points, :riderkey]]
 
     return rider_df
 end
 
+
 """
-`create_key` creates a unique key for each rider based on their name.
+## `getvgriders`
+
+This function downloads and parses the rider listing for a specific race from the Velogames website.
+
+The function returns a DataFrame with the following columns:
+
+    * `name` - the name of the rider
+    * `team` - the team the rider rides for
+    * `rank` - the rank of the rider in the category
+    * `score` - the number of points scored by the rider
 """
-create_key(arr) =
-    map(arr) do x
-        s = replace(x, r"[^a-zA-Z0-9_]" => "")
-        join(sort(collect(Unicode.normalize(s, stripmark=true, stripcc=true, casefold=true))))
+function getvgriders(pageurl::String)
+    # download the page and parse the table
+    rider_df = gettable(pageurl)
+
+    # normalise class data, if it exists
+    if hasproperty(rider_df, :class)
+        for class in unique(rider_df.class)
+            rider_df[!, class] = rider_df.class .== class
+        end
     end
+
+    # calculate the value of the rider
+    rider_df.value = rider_df.points ./ rider_df.cost
+
+    return rider_df
+end
