50c7321aa8c96d5d45f64314c9c9067a4be4b0b5
jz <james.zuccollo@epi.org.uk>
2023-05-09 21:13:26 +0100

    Simplify data reading code

--- src/get_data.jl
+++ src/get_data.jl
@@ -1,8 +1,22 @@
-using DataFrames
-using Cascadia
-using Gumbo
-using CategoricalArrays
-using Unicode
+using HTTP, DataFrames, Cascadia, Gumbo, CategoricalArrays, Unicode
+
+const PCS_RANKING_URLS = Dict(
+    :overall => "https://www.procyclingstats.com/rankings.php",
+    :oneday => "https://www.procyclingstats.com/rankings/me/one-day-races",
+    :gc => "https://www.procyclingstats.com/rankings/me/gc-ranking",
+    :sprint => "https://www.procyclingstats.com/rankings/me/sprinters",
+    :mountain => "https://www.procyclingstats.com/rankings/me/climbers",
+    :tt => "https://www.procyclingstats.com/rankings/me/time-trial"
+)
+
+const VG_ROWS = Dict(
+    :name => "https://www.procyclingstats.com/rankings.php",
+    :team => "https://www.procyclingstats.com/rankings/me/one-day-races",
+    :startlist => "https://www.procyclingstats.com/rankings/me/gc-ranking",
+    :cost => "https://www.procyclingstats.com/rankings/me/sprinters",
+    :score => "https://www.procyclingstats.com/rankings/me/climbers",
+    :tt => "https://www.procyclingstats.com/rankings/me/time-trial"
+)
 
 """
 ## `getvelogamesriders`
@@ -18,41 +32,36 @@ The function returns a DataFrame with the following columns:
 * `score` - the number of points scored by the rider in this competition
 * `value` - the number of points scored per unit cost
 """
-function getvelogamesriders(pageurl::String)
-    page = parsehtml(read(download(pageurl), String))
+function getvelogamesriders(pageurl::AbstractString)
+    page = HTTP.get(pageurl) |> String |> parsehtml
     rider_table = eachmatch(sel"table", page.root)[1]
-    rider_names = String[]
-    rider_startlist = String[]
-    rider_team = String[]
-    rider_costs = Int64[]
-    rider_scores = Int64[]
-    for rider_row in eachmatch(sel"tr", rider_table)[2:end]
-        rider_cells = eachmatch(sel"td", rider_row)
-        push!(rider_names, text(rider_cells[2]))
-        push!(rider_startlist, text(rider_cells[4]))
-        push!(rider_team, text(rider_cells[3]))
-        push!(rider_scores, parse(Int64, text(rider_cells[5])))
-        push!(rider_costs, parse(Int64, text(rider_cells[6])))
-    end
+
     rider_df = DataFrame(
-        name=rider_names,
-        team=categorical(rider_team),
-        startlist=categorical(rider_startlist),
-        cost=rider_costs,
-        score=rider_scores,
-        riderkey=create_key(rider_names)
+        name=String[],
+        cost=Int64[],
+        team=categorical(String[]),
+        score=Int64[],
+        startlist=categorical(String[]),
     )
 
-    # add a column of the score per cost
-    rider_df.value = rider_df.score ./ rider_df.cost
-
-    # check that the riderkey is unique
-    if length(unique(rider_df.riderkey)) != length(rider_df.riderkey)
-        error("Rider keys are not unique")
+    for rider_row in eachmatch(sel"tr", rider_table)[2:end]
+        rider_cells = eachmatch(sel"td", rider_row)
+        push!(rider_df.score, parse(Int64, text(rider_cells[5])))
+        push!(rider_df.name, text(rider_cells[2]))
+        push!(rider_df.team, text(rider_cells[3]))
+        push!(rider_df.cost, parse(Int64, text(rider_cells[6])))
+        push!(rider_df.startlist, text(rider_cells[4]))
     end
 
-    return rider_df
+    # calculate the value of the rider
+    rider_df.value = rider_df.score ./ rider_df.cost
 
+    # add a riderkey column based on the name
+    rider_df.riderkey = create_key(rider_df.name)
+    # check that the riderkey is unique
+    @assert length(unique(rider_df.riderkey)) == length(rider_df.riderkey) "Rider keys are not unique"
+
+    return rider_df
 end
 
 
@@ -106,50 +115,34 @@ The function returns a DataFrame with the following columns:
     * `score` - the number of points scored by the rider
 """
 function getpcsranking(category::Symbol)
-    rankingurl = (
-        overall="https://www.procyclingstats.com/rankings.php",
-        oneday="https://www.procyclingstats.com/rankings/me/one-day-races",
-        gc="https://www.procyclingstats.com/rankings/me/gc-ranking",
-        sprint="https://www.procyclingstats.com/rankings/me/sprinters",
-        mountain="https://www.procyclingstats.com/rankings/me/climbers",
-        tt="https://www.procyclingstats.com/rankings/me/time-trial",
-    )
-
     # check that the category is valid
-    if !(category in keys(rankingurl))
-        error("Invalid category: $category")
-    end
+    haskey(PCS_RANKING_URLS, category) || error("Invalid category: $category")
 
     # download the page and parse the table
-    page = parsehtml(read(download(rankingurl[category]), String))
+    page = HTTP.get(PCS_RANKING_URLS[category]).body |> String |> parsehtml
     rider_table = eachmatch(sel"table", page.root)[1]
 
     # parse the table to a DataFrame
-    rider_rank = Int64[]
-    rider_team = String[]
-    rider_name = String[]
-    rider_points = Int64[]
-
-    for rider_row in eachmatch(Selector("tr"), rider_table)[2:end]
-        rider_cells = eachmatch(Selector("td"), rider_row)
-        push!(rider_name, text(rider_cells[4]))
-        push!(rider_team, text(rider_cells[5]))
-        push!(rider_rank, parse(Int64, text(rider_cells[1])))
-        push!(rider_points, parse(Int64, text(rider_cells[6])))
-    end
-
     rider_df = DataFrame(
-        name=rider_name,
-        team=categorical(rider_team),
-        rank=rider_rank,
-        points=rider_points,
-        riderkey=create_key(rider_name)
+        rank=Int64[],
+        name=String[],
+        team=String[],
+        points=Int64[]
     )
 
-    # check that the riderkey is unique
-    if length(unique(rider_df.riderkey)) != length(rider_df.riderkey)
-        error("Rider keys are not unique")
+    for rider_row in eachmatch(sel"tr", rider_table)[2:end]
+        rider_cells = eachmatch(sel"td", rider_row)
+        push!(rider_df.rank, parse(Int64, text(rider_cells[1])))
+        push!(rider_df.name, text(rider_cells[4]))
+        push!(rider_df.team, text(rider_cells[5]))
+        push!(rider_df.points, parse(Int64, text(rider_cells[6])))
     end
+
+    # add a riderkey column based on the name
+    rider_df.riderkey = create_key(rider_df.name)
+
+    # check that the riderkey is unique
+    length(unique(rider_df.riderkey)) == length(rider_df.riderkey) || error("Rider keys are not unique")
 
     return rider_df
 end
@@ -159,8 +152,6 @@ end
 """
 create_key(arr) =
     map(arr) do x
-        s = string(x)
-        s = Unicode.normalize(s, stripmark=true, stripcc=true, casefold=true)
-        s = replace(s, r"[^a-zA-Z0-9_]" => "")
-        s = join(sort(collect(s)))
+        s = replace(x, r"[^a-zA-Z0-9_]" => "")
+        join(sort(collect(Unicode.normalize(s, stripmark=true, stripcc=true, casefold=true))))
     end
