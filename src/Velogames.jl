fc0883fc0dcea6895a0eee514c134feb12cde2a9
jz <james.zuccollo@epi.org.uk>
2024-03-08 11:49:50 +0000

    Update notebooks
    What a mess, just bunging everything in here.

--- src/Velogames.jl
+++ src/Velogames.jl
@@ -1,7 +1,7 @@
 module Velogames
 
 using HTTP, DataFrames, TableScraper, Cascadia, Gumbo, CategoricalArrays, Unicode, HiGHS, JuMP
-export solverace, getvgriders, getvgracepoints, getpcsriderpts, getpcsriderhistory, getpcsranking, getodds, buildmodeloneday, buildmodelstage
+export solverace, getvgriders, getvgracepoints, getpcsriderpts, getpcsriderhistory, getpcsranking, getpcsraceranking, getodds, buildmodeloneday, buildmodelstage
 
 include("build_model.jl")
 include("get_data.jl")
--- src/build_model.jl
+++ src/build_model.jl
@@ -30,13 +30,13 @@ Build the optimisation model for the velogames game.
 - `riderdf::DataFrame`: the rider data
 
 """
-function buildmodelstage(inputdf::DataFrame)
+function buildmodelstage(inputdf::DataFrame, n::Integer, points::Symbol, cost::Symbol)
     model = JuMP.Model(HiGHS.Optimizer)
     JuMP.set_silent(model)
     JuMP.@variable(model, x[inputdf.rider], Bin)
-    JuMP.@objective(model, Max, inputdf.calcscore' * x) # maximise the total score
-    JuMP.@constraint(model, inputdf.cost' * x <= 100) # cost must be <= 100
-    JuMP.@constraint(model, sum(x) == 9) # exactly 9 riders must be chosen
+    JuMP.@objective(model, Max, inputdf[!, points]' * x) # maximise the total score
+    JuMP.@constraint(model, inputdf[!, cost]' * x <= 100) # cost must be <= 100
+    JuMP.@constraint(model, sum(x) == n) # exactly n riders must be chosen
     JuMP.@constraint(model, inputdf[!, "allrounder"]' * x >= 2) # at least 2 must be all rounders
     JuMP.@constraint(model, inputdf[!, "sprinter"]' * x >= 1) # at least 1 must be a sprinter
     JuMP.@constraint(model, inputdf[!, "climber"]' * x >= 2) # at least 2 must be climbers
