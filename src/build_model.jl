16e6336ccb9a3df63da135da386ee3f80442274b
jz <james.zuccollo@epi.org.uk>
2024-04-08 10:26:39 +0100

    Allow cost of team to vary

--- src/build_model.jl
+++ src/build_model.jl
@@ -6,14 +6,13 @@ Build the optimisation model for the velogames game.
 - `riderdf::DataFrame`: the rider data
 
 """
-function buildmodeloneday(inputdf::DataFrame, n::Integer, points::Symbol, cost::Symbol)
+function buildmodeloneday(inputdf::DataFrame, n::Integer, points::Symbol, cost::Symbol; totalcost::Integer=100)
     model = JuMP.Model(HiGHS.Optimizer)
     JuMP.set_silent(model)
     JuMP.@variable(model, x[inputdf.rider], Bin)
-    JuMP.@objective(model, Max, inputdf[!, points]' * x) # maximise the total score
-    JuMP.@constraint(model, inputdf[!, cost]' * x <= 100) # cost must be <= 100
+    JuMP.@objective(model, Max, inputdf[!, points]' * x) # maximize the total score
+    JuMP.@constraint(model, inputdf[!, cost]' * x <= totalcost) # cost must be <= totalcost
     JuMP.@constraint(model, sum(x) == n) # exactly n riders must be chosen
-    # @constraint(model, ) # exactly n teams must be chosen
     optimize!(model)
     if termination_status(model) != OPTIMAL
         @warn("The model was not solved correctly.")
@@ -21,6 +20,7 @@ function buildmodeloneday(inputdf::DataFrame, n::Integer, points::Symbol, cost::
     end
     return JuMP.value.(x)
 end
+
 
 """
     buildmodelstage(riderdf::DataFrame)
